abf492372a31bb1c6129fee95ee0fd671e403c06
hirak729 <hirakjyoti1233@gmail.com>
2025-02-27 00:10:05 +0530

    user signup login and logout task added

--- client/login.php
+++ client/login.php
@@ -1,16 +1,16 @@
 <div class="container">
     <h1 class="heading mt-3">Login</h1>
-    <form>
+    <form action="./server/requests.php" method="post">
         <div class="col-6 offset-sm-3 mb-3">
             <label for="email" class="form-label">User Email</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter your email">
+            <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email">
         </div>
         <div class="col-6 offset-sm-3 mb-3">
             <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" placeholder="Enter password">
+            <input type="password" name="password" class="form-control" id="password" placeholder="Enter password">
         </div>
         <div class="col-6 offset-sm-3">
-        <button type="submit" class="btn btn-primary">Login</button>
+        <button type="submit" name="login" class="btn btn-primary">Login</button>
         </div>
     </form>
 </div>
--- server/requests.php
+++ server/requests.php
@@ -21,4 +21,27 @@ include('../common/db.php');
             echo "User registration failed";
         }
     }
+    elseif(isset($_POST['login'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $username = "";
+        // print_r($_POST);
+        $query = "SELECT * FROM `users` WHERE email='$email' AND password='$password'";
+        $result = $conn->query($query);
+        if($result->num_rows == 1){
+            
+            foreach($result as $row){
+                $username = $row['username'];
+                $email = $row['email'];
+            }
+            $_SESSION['user'] = ["username"=>$username, "email"=>$email];
+            header("location: /php projects/discuss");
+        }
+    }
+    elseif(isset($_GET['logout'])){
+        session_unset();
+        header("location: /php projects/discuss");
+    }
+
+
 ?>
